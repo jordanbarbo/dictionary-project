b729116c3e406f2b6fe8451c2d2eb9eb09101c7f
jordanbarbo <jordanbarbo@gmail.com>
2021-09-01 17:28:03 -0700

    Updated Links

--- src/App.js
+++ src/App.js
@@ -12,17 +12,26 @@ function App() {
         </main>
         <footer>
           This project was coded by{" "}
-          <a href="https://www.linkedin.com/in/groundjordan/" target="_blank">
+          <a
+            href="https://www.linkedin.com/in/groundjordan/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Jordan Barbo
           </a>{" "}
           and is open-sourced on{" "}
-          <a href="https://github.com/jordanbarbo" target="_blank">
+          <a
+            href="https://github.com/jordanbarbo"
+            target="_blank"
+            rel="noreferrer"
+          >
             GitHub
           </a>{" "}
           and hosted on{" "}
           <a
             href="https://app.netlify.com/sites/trusting-villani-32b6a1/overview"
             target="_blank"
+            rel="noreferrer"
           >
             Netlify
           </a>
