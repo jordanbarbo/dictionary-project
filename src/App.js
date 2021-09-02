9f1fe27f646a68c9040399d6444bd10bf453ce8f
jordanbarbo <jordanbarbo@gmail.com>
2021-09-01 17:00:56 -0700

    Added Footer

--- src/App.js
+++ src/App.js
@@ -3,9 +3,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
+      <div className="container">
+        <header className="App-header">Hello</header>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.linkedin.com/in/groundjordan/" target="_blank">
+            Jordan Barbo
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a href="https://github.com/jordanbarbo" target="_blank">
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://app.netlify.com/sites/trusting-villani-32b6a1/overview"
+            target="_blank"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
