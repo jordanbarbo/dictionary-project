04e37e3285c67e448efc06ea44cf1d3fc376183c
jordanbarbo <jordanbarbo@gmail.com>
2021-09-01 17:17:09 -0700

    Added Input Form with State

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Dictionary() {
-  return "Hello from Dictionary";
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+
+  function handleKeywordChange(event) {
+    event.preventDefault();
+    setKeyword(event.target.value);
+  }
+    
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  )
 }
