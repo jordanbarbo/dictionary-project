b5852ac59d80b519e072a21ab64d009ab7335aef
jordanbarbo <jordanbarbo@gmail.com>
2021-09-22 10:35:16 -0700

    Added search results

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results.js";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
+    setResults(response.data[0]);
   }
 
   // documentation: https://dictionaryapi.dev/
@@ -26,6 +29,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
